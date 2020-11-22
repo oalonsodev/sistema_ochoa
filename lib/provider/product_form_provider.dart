e5be82d59f1178fab5ac60846ac4c7459017bdf3
Oscar Alonso <oscar.alonso.hdez@outlook.com>
2020-11-22 16:06:14 -0600

    Trabajando en error: formularios invisibles excepto el 1ro. ✔: ProductFormProvider: comentarios convertidos en documentación. 📌: CotProductPage: Aplicar sustitución de GlobalKey por lista de GlobalKeys ProductForm: Aplicar sustitución de GlobalKey por lista de GlobalKeys. ❌: Salto automático al último Tab al terminar de escribir en TextFormField.

--- lib/provider/product_form_provider.dart
+++ lib/provider/product_form_provider.dart
@@ -1,36 +1,36 @@
 import 'package:flutter/material.dart';
 
 class ProductFormProvider with ChangeNotifier {
-  //* Lista de GlobalKeys.
+  /// Lista de GlobalKeys.
   List<GlobalKey<FormState>> _formKeyList = [new GlobalKey<FormState>()];
 
-  //* obtener la lista de GlobalKeys.
+  /// obtener la lista de GlobalKeys.
   List<GlobalKey<FormState>> get getFormKeyList {
     return _formKeyList;
   }
 
-  //* Agregar un GlobalKey a la lista.
+  /// Agregar un GlobalKey a la lista.
   void addGlobalKey() {
     _formKeyList.add(new GlobalKey<FormState>());
 
     notifyListeners();
   }
 
-  //* Remover un GlobalKey de la lista.
+  /// Remover un GlobalKey de la lista.
   void removeGlobalKey(int index) {
     _formKeyList.remove(index);
 
     notifyListeners();
   }
 
-  //* Ejecutar método save de un determinado formulario mediante su GlobalKey.
+  /// Ejecutar método save de un determinado formulario mediante su GlobalKey.
   void saveForm(int index) {
     _formKeyList[index].currentState.save();
 
     notifyListeners();
   }
 
-  //* Validar los campos de un determinado formulario mediante su GlobalKey.
+  /// Validar los campos de un determinado formulario mediante su GlobalKey.
   bool formIsValid(int index) {
     return _formKeyList[index].currentState.validate();
   }
